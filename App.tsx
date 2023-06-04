e08fa4b6ac2e97d42cd8c32c7d0116de4731676d
Jun-10-r <junior.codemaster@gmail.com>
2023-06-03 23:00:10 -0300

    Criacao da calculadora de media

--- App.tsx
+++ App.tsx
@@ -1,20 +1,84 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect } from "react";
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
-export default function App() {
+export default function Home() {
+  const [av1, setAv1] = useState('2');
+  const [av2, setAv2] = useState('8');
+  const [media, setMedia] = useState('0');
+
+  useEffect(() => {
+    const total =(Number(av1) + Number(av2)) / 2;
+    setMedia(String(total));
+  }, [av1, av2]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Cálculo da Média</Text>
+      <View style={styles.box}>
+        <Text style={styles.text}>AV1</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={setAv1}
+          value={String(av1)}
+          />
     </View>
-  );
+    <View style={styles.box}>
+      <Text style={styles.text}>AV2</Text>
+      <TextInput
+      style={styles.input}
+      onChangeText={setAv2}
+      value={String(av2)}
+      />
+    </View>
+    <View style={styles.box}>
+      <Text style={styles.title}>
+        Média {media} {Number(media) >= 7 ? 'Aprovado!' : Number(media) < 4 ? 'Reprovado!' : 'Prova final'}
+      </Text>
+    </View>
+    {(Number(media) >= 4) && (Number(media) < 7) && (
+      <View style={styles.box}>
+        <Text style={styles.text}>Precisa tirar no mínimo: {10 - Number(media)}</Text>
+      </View>
+    )}
+    </View>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const styles = StyleSheet.create({
+    container:{
+      marginTop: 25,
+      flex: 1,
+      width:'100%',
+      padding: 10,
+      backgroundColor: '#325',
+    },
+    title: {
+      color: '#fff',
+      textAlign: 'center',
+      textTransform: 'uppercase',
+      fontSize: 24,
+    },
+    box:{
+      flexDirection: 'row',
+      alignItems: 'center',
+      margin: 10,
+      padding: 10,
+      borderWidth: 1,
+      borderColor: '#aaa',
+      borderRadius: 10,
+    },
+    text:{
+      marginRight: 15,
+      color: '#fff',
+      fontSize: 18,
+    },
+    input: {
+      flex: 1,
+      borderWidth: 1,
+      borderColor: '#aaa',
+      borderRadius: 10,
+      padding: 5,
+      backgroundColor: '#333',
+      color: '#fff',
+      fontSize: 18,
+    }
+  })
